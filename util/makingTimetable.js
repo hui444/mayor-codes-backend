8033008ae57cda405d352c844c13d9714e929543
Greek <ygreek@gmail.com>
2020-07-19 01:50:21 +0800

    Edited code to adapt to special cases

--- util/makingTimetable.js
+++ util/makingTimetable.js
@@ -1,4 +1,4 @@
-const makingTimetable = (data) => {
+const makingTimetable = (moduleCode, data) => {
   var lessons = [],
     classes = [];
 
@@ -24,80 +24,73 @@ const makingTimetable = (data) => {
     recClassMust = [];
 
   while (data[i]) {
-    if (data[i].lessonType === "Tutorial") {
-      tutorialType[tut] = data[i];
-      tut++;
-    } else if (data[i].lessonType === "Lecture") {
-      lectureType[lec] = data[i];
-      lec++;
-    } else if (data[i].lessonType === "Sectional Teaching") {
-      sectionalType[sec] = data[i];
-      sec++;
-    } else if (data[i].lessonType === "Laboratory") {
-      laboratoryType[lab] = data[i];
-      lab++;
-    } else if (data[i].lessonType === "Recitation") {
-      recitationType[rec] = data[i];
-      rec++;
-    } else {
-      console.log(data[i]);
-      break;
-    } //not of known type/undefined
+    if (!(data[i].day === "Saturday" || data[i].day === "Sunday")) {
+      //CHECK IF THE CLASSNO ARE THE SAME!!!!!!!!!!
+      if (data[i].lessonType === "Tutorial") {
+        tutorialType[tut] = data[i];
+        tut++;
+      } else if (data[i].lessonType === "Lecture") {
+        lectureType[lec] = data[i];
+        lec++;
+      } else if (data[i].lessonType === "Sectional Teaching") {
+        sectionalType[sec] = data[i];
+        sec++;
+      } else if (data[i].lessonType === "Laboratory") {
+        laboratoryType[lab] = data[i];
+        lab++;
+      } else if (data[i].lessonType === "Recitation") {
+        recitationType[rec] = data[i];
+        rec++;
+      } else {
+        console.log(data[i]);
+        break;
+      } //not of known type/undefined
+    }
     i++;
   }
 
   if (tut) {
     if (tut === 1) {
-      tutClassMust[0] = tutorialType[0].classNo;
-    } else {
-      tutClassMust[0] = "Please Choose tutorial";
+      tutClassMust[0] = moduleCode + " Tutorial " + tutorialType[0].classNo;
     }
     ++j;
     lessons[j] = "Tut";
   }
   if (lec) {
     if (lec === 1) {
-      lecClassMust[0] = lectureType[0].classNo;
-    } else {
-      lecClassMust[0] = "Please Choose lecture";
+      lecClassMust[0] = moduleCode + " Lecture " + lectureType[0].classNo;
     }
     ++j;
     lessons[j] = "Lec";
   }
   if (sec) {
     if (sec === 1) {
-      secClassMust[0] = sectionalType[0].classNo;
-    } else {
-      secClassMust[0] = "Please Choose sectional";
+      secClassMust[0] = moduleCode + " Sectional " + sectionalType[0].classNo;
     }
     ++j;
     lessons[j] = "Sec";
   }
   if (lab) {
     if (lab === 1) {
-      labClassMust[0] = laboratoryType[0].classNo;
-    } else {
-      labClassMust[0] = "Please Choose laboratory";
+      labClassMust[0] = moduleCode + " Laboratory " + laboratoryType[0].classNo;
     }
     ++j;
     lessons[j] = "Lab";
   }
   if (rec) {
     if (rec === 1) {
-      recClassMust[0] = recitationType[0].classNo;
-    } else {
-      recClassMust[0] = "Please Choose recitation";
+      recClassMust[0] = moduleCode + " Recitation " + recitationType[0].classNo;
     }
     ++j;
     lessons[j] = "Rec";
   }
 
   //fill classes array with compulsory lessons (no choosing)
-  if (tutClassMust[0]) classes.push(tutClassMust);
-  if (lecClassMust[0]) classes.push(lecClassMust);
-  if (secClassMust[0]) classes.push(secClassMust);
-  if (labClassMust[0]) classes.push(labClassMust);
-  if (recClassMust[0]) classes.push(recClassMust);
+  if (tutClassMust[0]) classes = [].concat(classes, tutClassMust);
+  if (lecClassMust[0]) classes = [].concat(classes, lecClassMust);
+  if (secClassMust[0]) classes = [].concat(classes, secClassMust);
+  if (labClassMust[0]) classes = [].concat(classes, labClassMust);
+  if (recClassMust[0]) classes = [].concat(classes, recClassMust);
 
   return {
     lessons,

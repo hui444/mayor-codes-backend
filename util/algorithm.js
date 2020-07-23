d87237957d52557800577e01c37eb21aec4f17af
Greek <ygreek@gmail.com>
2020-07-23 14:19:32 +0800

    Started algo, choose slots for "Choose" and deleted timings of selected slots. Added checkClash to deleteTime.js file. Algo does not check is preferred slots clash

--- util/algorithm.js
+++ util/algorithm.js
@@ -1,7 +1,8 @@
 const deleteTime = require("./deleteTime");
 const whichDay = require("./whichDay");
+const getInfoForModuleCode = require("./moduleInfo");
 
-const algorithm = (data) => {
+const algorithm = async (data) => {
   var mon = [
       0800,
       0900,
@@ -83,23 +84,47 @@ const algorithm = (data) => {
       2100,
     ];
 
-  var week_arr = [mon, tue, wed, thu, fri];
+  var week_arr = [mon, tue, wed, thu, fri],
+    NEW_week_arr;
   //mod1 takes priority
+
+  let j = 0;
+  let bool = true;
+
   console.log(data.extractedData1.tutDetails);
   if (data.extractedData1.tutDetails) {
     for (let i = 0; i < data.extractedData1.tutDetails.length; i++) {
       if (data.extractedData1.tutDetails === "Choose") {
-        break;
+        while (bool) {
+          data.extractedData1.tutDetails = [
+            (await getInfoForModuleCode(data.extractedData1.modCode)).tutInfo[
+              j
+            ],
+          ];
+          console.log(data.extractedData1.tutDetails[i]);
+          NEW_week_arr = deleteTime(
+            week_arr,
+            whichDay(data.extractedData1.tutDetails[i]),
+            data.extractedData1.tutDetails[i],
+            true
+          );
+          if (NEW_week_arr) {
+            week_arr = NEW_week_arr;
+            bool = false;
+          } else j++;
+        }
       } else {
+        //did not check clashes in preferred slots
         week_arr = deleteTime(
           week_arr,
           whichDay(data.extractedData1.tutDetails[i]),
-          data.extractedData1.tutDetails[i]
+          data.extractedData1.tutDetails[i],
+          false
         );
       }
     }
   }
-  //   console.log(week_arr);
+  console.log(week_arr);
 };
 
 module.exports = algorithm;
--- util/deleteTime.js
+++ util/deleteTime.js
@@ -1,22 +1,38 @@
-const deleteTime = (weekArr, day, classDetails) => {
+const deleteTime = (weekArr, day, classDetails, checkClash) => {
   var duration;
 
-  if (weekArr[day].includes(parseInt(classDetails.startTime))) {
+  if (checkClash) {
+    if (weekArr[day].includes(parseInt(classDetails.startTime))) {
+      duration = classDetails.endTime - classDetails.startTime;
+      weekArr[day] = weekArr[day].filter((time) => {
+        return time !== parseInt(classDetails.startTime);
+      });
+      for (let i = 100; i < duration; i += 100) {
+        var calTime = parseInt(classDetails.startTime) + i;
+        if (!weekArr[day].includes(calTime)) return null;
+        else
+          weekArr[day] = weekArr[day].filter((time) => {
+            return time !== calTime;
+          });
+      }
+    } else return null;
+
+    return weekArr;
+  } else {
     duration = classDetails.endTime - classDetails.startTime;
     weekArr[day] = weekArr[day].filter((time) => {
       return time !== parseInt(classDetails.startTime);
     });
     for (let i = 100; i < duration; i += 100) {
       var calTime = parseInt(classDetails.startTime) + i;
-      if (!weekArr[day].includes(calTime)) return null;
-      else
-        weekArr[day] = weekArr[day].filter((time) => {
-          return time !== calTime;
-        });
-    }
-  } else return null;
 
-  return weekArr;
+      weekArr[day] = weekArr[day].filter((time) => {
+        return time !== calTime;
+      });
+    }
+
+    return weekArr;
+  }
 };
 
 module.exports = deleteTime;
